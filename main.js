a7fb4ab90a2cc8eb8446c1d7f4d08478485d0f66
yorleX23 <bogdandzikovskiy@gmail.com>
2025-09-21 19:45:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-// main.js
-// Варіант 5: міжнародні резерви, дата лютий 2022
+
 
 const { URL } = require('url');
 
 // Константа (тут має бути дата у форматі, який приймає НБУ)
-const date = "20220201"; // 1 лютого 2022 (формат YYYYMMDD)
+const date = "20220201";  (формат YYYYMMDD)
 
 // Базовий URL без параметрів
 const myUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/reserves");
@@ -13,5 +12,5 @@ const myUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/reser
 myUrl.searchParams.append("date", date);
 myUrl.searchParams.append("json", "");
 
-// Виводимо у консоль готовий URL
+
 console.log(myUrl.toString());
