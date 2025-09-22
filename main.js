693d3f4d634c0be99d3d10a3884cf74855e2a6d9
yorleX23 <bogdandzikovskiy@gmail.com>
2025-09-22 13:28:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const { URL } = require('url');
 const date = "20220201";  (формат YYYYMMDD)
 
 // Базовий URL без параметрів
-const myUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/reserves");
+const myUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/res");
 
 // Додаємо параметри
 myUrl.searchParams.append("date", date);
